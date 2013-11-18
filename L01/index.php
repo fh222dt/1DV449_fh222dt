cd7bde138541e4bcc2945811f8433796126ad225
fh222dt <fh222dt@student.lnu.se>
2013-11-18 13:11:32 +0100

    cookies

--- L01/index.php
+++ L01/index.php
@@ -2,13 +2,35 @@
 	function curl($url) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+
+		$post_array = array(
+			"username" => "admin",
+			"password" => "admin"
+		);
+
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_array);
+
+		//felsökningshjälp
+		//curl_setopt($ch, CURLOPT_HEADER, 1);
+		//curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+		//
+
+		curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__) . "/cookie.txt");
+
 		$data = curl_exec($ch);
 		curl_close($ch);
-		return $data;
-	
+
+		//leta reda på location
+		$location ="";
+		if(preg_match('#Location: (.*)#', $data, $r)){
+			$location = trim($r[1]);
+		}
+		
+		return $data;	
 	}
 
-	$scraped = curl("http://vhost3.lnu.se:20080/~1dv449/scrape/");
+	$scraped = curl("http://vhost3.lnu.se:20080/~1dv449/scrape/check.php/");
 
 	var_dump($scraped);
