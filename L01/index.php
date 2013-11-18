67bd7f2bb0f93707ceaa6f773b59eb6343e23f2c
fh222dt <fh222dt@student.lnu.se>
2013-11-18 16:18:51 +0100

    kan logga in & se resultatlista

--- L01/index.php
+++ L01/index.php
@@ -1,36 +1,36 @@
 <?php
 	function curl($url) {
+		$useragent = "MyScraper v0.1";		//så att webmastern kan blocka skrapor enkelt
+
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);	// Setting URL to POST to
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);	// Returning transfer as a string
+		curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE);	// Use cookies
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);	// Follow Location: headers
+		curl_setopt($ch, CURLOPT_COOKIEFILE, dirname(__FILE__) . "cookie.txt");	// Setting cookiefile
+		curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__) . "cookie.txt");	// Setting cookiejar
+		curl_setopt($ch, CURLOPT_USERAGENT, $useragent);	// Setting useragent
+		curl_setopt($ch, CURLOPT_URL, $url);	// Setting URL to POST to
+		curl_setopt($ch, CURLOPT_POST, 1);		// Setting method as POST
 
 		$post_array = array(
 			"username" => "admin",
 			"password" => "admin"
 		);
 
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_array);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post_array);		
 
 		//felsökningshjälp
 		//curl_setopt($ch, CURLOPT_HEADER, 1);
 		//curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
 		//
 
-		curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__) . "/cookie.txt");
-
 		$data = curl_exec($ch);
 		curl_close($ch);
-
-		//leta reda på location
-		$location ="";
-		if(preg_match('#Location: (.*)#', $data, $r)){
-			$location = trim($r[1]);
-		}
 		
 		return $data;	
 	}
 
-	$scraped = curl("http://vhost3.lnu.se:20080/~1dv449/scrape/check.php/");
+	$scraped = curl("http://vhost3.lnu.se:20080/~1dv449/scrape/check.php");
 
 	var_dump($scraped);
