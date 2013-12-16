12aaf4f09804b4945af3e4341d45e067a12eb360
fh222dt <fh222dt@student.lnu.se>
2013-12-16 11:37:33 +0100

    alla markers finns

--- L03/index.php
+++ L03/index.php
@@ -3,80 +3,24 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-
-    <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map-canvas { height: 100% }
-    </style>
-
+    <link rel="stylesheet" href="custom.css">
     <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtJs5Lz6qV0SnxxE_tEumFq4CaB54g-S0&sensor=true">
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtJs5Lz6qV0SnxxE_tEumFq4CaB54g-S0&sensor=false">
     </script>
 
     <script type="text/javascript">
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+      
     </script>
 
 
   </head>
   <body>
-    <div id="map-canvas"/>
+    <header><h1>Trafikrapport</h1></header>
+    
+    <div id='all-messages' class='pull-right'> Senaste 100 meddelanden:</div>
+    <div id="map-canvas" class='pull-left'/>
+
     <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-
-    <script type="text/javascript">
-    	/*$.getJSON( "http://api.sr.se/api/v2/traffic/messages?format=json, callback =success&indent=true&page=1", function( data ) {
-		  var items = [];
-		  $.each( data, function( key, val ) {
-		    items.push( "<li id='" + key + "'>" + val + "</li>" );
-		  });
-		 
-		  $( "<ul/>", {
-		    "class": "my-new-list",
-		    html: items.join( "" )
-		  }).appendTo( "body" );
-		});*/
-
-		
-
-/*(function($) {
-var url = 'http://api.sr.se/api/v2/traffic/messages?format=json&callback=?&indent=true&pagination=false';
-
-$.ajax({
-	type: "GET",
-	url: url,
-	jsonpCallback: 'jsonCallback',
-	contentType: "application/json",
-	dataType: 'jsonp',
-	success: function(json) {
-	console.dir(json.messages);
-  console.dir(json.pagination);
-	},
-	error: function(e) {
-	console.log(e.message);
-	}
-});
-
-})(jQuery);
-
-jsonCallback(
-    {
-        "messages": [],
-        "pagination": {}
-
-    }
-);
-*/
-
-    </script>
     <script src="traffic.js"></script>
 
   </body>
--- L03/traffic.js
+++ L03/traffic.js
@@ -5,24 +5,82 @@ var FT = FT || {};
 var url = 'http://api.sr.se/api/v2/traffic/messages?format=json&callback=?&indent=true&pagination=false';
 var maxMessages = 100;
 
-$.ajax({
-	type: "GET",
-	url: url,
-	jsonpCallback: 'jsonCallback',
-	contentType: "application/json",
-	dataType: 'jsonp',
-	success: function(json) {
-		var messages = json.messages;
-		var 
+	$.ajax({
+		type: "GET",
+		url: url,
+		jsonpCallback: 'jsonCallback',
+		contentType: "application/json",
+		dataType: 'jsonp',
+		/*success: function(json) {
+			var messages = json.messages;
+			messages.reverse();			//vänder på arrayen så nyast kommer först
 
-	},
-	error: function(e) {
-		console.log(e.message);
-	}
-});
+			 for (var i = 0; i < maxMessages; i++) {
+			 	$("<p>" +i + messages[i].title + "</p>").appendTo("body");
+			 }
+
+
+		},*/
+		error: function(e) {
+			console.log(e.message);
+		}
+	})
+		.done(function(json){
+			//console.log(json);
+
+			var messages = json.messages;
+			messages.reverse();			//vänder på arrayen så nyast kommer först
+
+			var mapOptions = {
+			          center: new google.maps.LatLng(59.32893, 18.06491),
+			          zoom: 8
+			        };
+			 	 
+		 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+			//presentera lista
+			 for (var i = 0; i < maxMessages; i++) {
+			 	$("<p>" +i +". "+ messages[i].title + "</p>").appendTo("#all-messages");
+			 }
+
+			 //placera ut markers
+			for (var i = 0; i < maxMessages; i++) {
+			 	
+
+			 	var content = '<h1>' + messages[i].title + '</h1>' +
+			 	 			'<p> Datum: ' + messages[i].createddate + '</p>' +
+			 	 			'<p> Beskrivning: ' + messages[i].description + '</p>' +
+			 	 			'<p> Kategori: ' + messages[i].category + '</p>';
+
+				var info = new google.maps.InfoWindow({
+					 content: content
+					});
+
+				var pos = new google.maps.LatLng(messages[i].latitude,messages[i].longitude);			 	 
+
+			 	var marker = new google.maps.Marker({
+			 	 	position: pos,
+			 	 	map: map,
+			 	 	title: messages[i].title,
+			 	 	infowindow: info
+			 	});
+
+  				//console.log(messages[i].latitude);
+  				
+  				google.maps.event.addListener(marker, 'click', function() {
+  					this.infowindow.close();
+    			this.infowindow.open(map,this);
+  				});
+
+  			
+
+			}
+
+			
+
+		});
 
 
 
 	
-
 })();
