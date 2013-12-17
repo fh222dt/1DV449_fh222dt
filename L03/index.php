6b713fb3342bb1570f08bf3d1174aea5a52735e6
fh222dt <fh222dt@student.lnu.se>
2013-12-17 12:33:13 +0100

    kategorier

--- L03/index.php
+++ L03/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="custom.css">
@@ -17,7 +18,15 @@
   <body>
     <header><h1>Trafikrapport</h1></header>
     
-    <div id='all-messages' class='pull-right'> Senaste 100 meddelanden:</div>
+    <div id='list' class='pull-right'>
+      <ul>
+        <li><a id='all-mess' href='#'>Alla meddelanden</a></li>
+        <li><a id='road' href='#'>Vägtrafik</a></li>
+        <li><a id='public' href='#'>Kollektivtrafik</a></li>
+        <li><a id='planed' href='#'>Planerad störning</a></li>
+        <li><a id='other' href='#'>Övrigt</a></li>
+      </ul>
+    </div>
     <div id="map-canvas" class='pull-left'/>
 
     <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
--- L03/traffic.js
+++ L03/traffic.js
@@ -1,9 +1,9 @@
 var FT = FT || {};
 
-(FT.start = function () {
+(FT.start = function () {	
 
-var url = 'http://api.sr.se/api/v2/traffic/messages?format=json&callback=?&indent=true&pagination=false';
-var maxMessages = 100;
+	var url = 'http://api.sr.se/api/v2/traffic/messages?format=json&callback=?&indent=true&pagination=false';
+	var maxMessages = 100;
 
 	$.ajax({
 		type: "GET",
@@ -11,70 +11,161 @@ var maxMessages = 100;
 		jsonpCallback: 'jsonCallback',
 		contentType: "application/json",
 		dataType: 'jsonp',
-		/*success: function(json) {
-			var messages = json.messages;
-			messages.reverse();			//vänder på arrayen så nyast kommer först
-
-			 for (var i = 0; i < maxMessages; i++) {
-			 	$("<p>" +i + messages[i].title + "</p>").appendTo("body");
-			 }
-
-
-		},*/
 		error: function(e) {
 			console.log(e.message);
+			$("<p>Just nu har vi inte kontakt med trafiksystemet. Vänligen" + 
+				"återkom vid ett senare tillfälle.</p>").appendTo("#body");
 		}
-	})
-		.done(function(json){
+	}).done(function(json){
 			//console.log(json);
 
-			var messages = json.messages;
-			messages.reverse();			//vänder på arrayen så nyast kommer först
+			var reports = json.messages;
+			reports.reverse();			//vänder på arrayen så nyast kommer först			
 
-			var mapOptions = {
-			          center: new google.maps.LatLng(59.32893, 18.06491),
-			          zoom: 8
-			        };
-			 	 
-		 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-		 	
+			//console.log(reports);
+
+			//kategorier
+			$("#all-mess").click(function() {
+  				CreateMarkers(reports);
+			});
+
+			$("#road").click(function() {
+				var road = [];
+				for (var i = 0; i < maxMessages; i++) {
+					if(reports[i].category === 0) {
+						road.push(reports[i]);
+					}
+				}
+  				CreateMarkers(road);
+  				console.log(road);
+			});
+
+			$("#public").click(function() {
+				var pTrans = [];
+				for (var i = 0; i < maxMessages; i++) {
+					if(reports[i].category === 1) {
+						pTrans.push(reports[i]);
+					}
+				}
+  				CreateMarkers(pTrans);
+			});
+
+			$("#planed").click(function() {
+				var planed = [];
+				for (var i = 0; i < maxMessages; i++) {
+					if(reports[i].category === 2) {
+						planed.push(reports[i]);
+					}
+				}
+  				CreateMarkers(planed);
+			});
+
+			$("#other").click(function() {
+				var other = [];
+				for (var i = 0; i < maxMessages; i++) {
+					if(reports[i].category === 3) {
+						other.push(reports[i]);
+					}
+				}
+  				CreateMarkers(other);
+			});	
+
+			//placera ut markers
+			CreateMarkers(reports);	
 
 			//presentera lista
-			 for (var i = 0; i < maxMessages; i++) {
-			 	$("<p>" +i +". "+ messages[i].title + "</p>").appendTo("#all-messages");
-			 }
-
-			 //placera ut markers
-
-			var infowindow = new google.maps.InfoWindow();
 			for (var i = 0; i < maxMessages; i++) {
-			 	
-
-			 	var content = '<h1>' + messages[i].title + '</h1>' +
-			 	 			'<p> Datum: ' + messages[i].createddate + '</p>' +
-			 	 			'<p> Beskrivning: ' + messages[i].description + '</p>' +
-			 	 			'<p> Kategori: ' + messages[i].category + '</p>';
-
-				 var info = new google.maps.InfoWindow({
-				 	 content: content
-				 	});
-
-				var pos = new google.maps.LatLng(messages[i].latitude,messages[i].longitude);			 	 
-
-			 	var marker = new google.maps.Marker({
-			 	 	position: pos,
-			 	 	map: map,
-			 	 	title: messages[i].title,
-			 	 	html: content
-			 	});
-
-  				//console.log(messages[i].latitude);
-  				
-				google.maps.event.addListener(marker, 'click', function() {
-               		infowindow.setContent(this.html);
-               		infowindow.open(map,this);
-       			});
+				$("<p>" +i +". "+ reports[i].title + "</p>").appendTo("#list");
 			}
+			
 		});
 	
 })();
+
+function CreateMarkers(messages){
+	var infowindow = new google.maps.InfoWindow();
+	var maxMessages = 100;
+
+	var mapOptions = {
+			          center: new google.maps.LatLng(59.32893, 18.06491),
+			          zoom: 5
+			        };
+			 	 
+ 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	for (var i = 0; i < maxMessages; i++) {
+	 	
+
+	 	var content = '<div id=' + 'infowindow' + '>' +
+	 				'<h1>' + messages[i].title + '</h1>' +
+	 	 			'<p> Datum: ' + getDate(messages[i].createddate) + '</p>' +
+	 	 			'<p> Beskrivning: ' + messages[i].description + '</p>' +
+	 	 			'<p> Kategori: ' + getCategory(messages[i].category) + '</p>' +
+	 	 			'</div>';
+
+		 var info = new google.maps.InfoWindow({
+		 	 content: content
+		 	});
+
+		var pos = new google.maps.LatLng(messages[i].latitude,messages[i].longitude);			 	 
+
+	 	var marker = new google.maps.Marker({
+	 	 	position: pos,
+	 	 	map: map,
+	 	 	title: messages[i].title,
+	 	 	html: content
+	 	});
+
+			//console.log(messages[i].latitude);
+			
+		google.maps.event.addListener(marker, 'click', function() {
+       		infowindow.setContent(this.html);
+       		infowindow.open(map,this);
+			});
+	}
+}
+
+function getCategory(no) {
+	var category;
+	switch (no) {
+		case 0:
+			category = "Vägtarfik";
+			break;
+		case 1:
+			category = "Kollektivtrafik";
+			break;
+		case 2:
+			category = "Planerad störning";
+			break;
+		case 3:
+			category = "Övrigt";
+			break;
+	} 
+	return category;
+}
+
+function getDate(jsonDate) {
+	var date = new Date(parseInt(jsonDate.substr(6)));
+	
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	if (month < 10) {
+		month = "0"+month;
+	}
+	var day = date.getDate();
+	if (day < 10) {
+		day = "0"+day;
+	}
+	var hour = date.getHours();
+	if (hour < 10) {
+		hour = "0"+hour;
+	}
+	var mins = date.getMinutes();
+	if (mins < 10) {
+		mins = "0"+mins;
+	}
+
+	date = year+'-'+month+'-'+day+', '+hour+':'+mins;
+
+	return date;
+}
