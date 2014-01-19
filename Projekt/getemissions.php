d7c78383969062291ce8719217bdab5c033c908c
fh222dt <fh222dt@student.lnu.se>
2014-01-19 20:57:58 +0100

    rapport

--- Projekt/getemissions.php
+++ Projekt/getemissions.php
@@ -1,12 +1,12 @@
 <?php
-require_once('Caching.php');
+require_once('caching.php');
 class getEmissions {	
 
 	function getStates(){
-		$states = [1=>"Blekinge", 2=>"Dalarnas", 3=>"Gotlands", 4=>"Gävleborgs", 5=>"Hallands", 6=>"Jämtlands", 7=>"Jönköpings", 
+		$states = array(1=>"Blekinge", 2=>"Dalarnas", 3=>"Gotlands", 4=>"Gävleborgs", 5=>"Hallands", 6=>"Jämtlands", 7=>"Jönköpings", 
 					8=>"Kalmar", 9=>"Kronobergs", 10=>"Norrbottens", 11=>"Skåne", 12=>"Stockholms", 13=>"Södermanlands", 14=>"Uppsala", 
 					15=>"Värmlands", 16=>"Västerbottens", 17=>"Västernorrlands", 18=>"Västmanlands", 19=>"Västra%20Götalands", 20=>"Örebro", 
-					21=>"Östergötlands"];
+					21=>"Östergötlands");
 		//hämta länsvis			
 		foreach ($states as $state){
 			$filename = $this->fileName($state);
